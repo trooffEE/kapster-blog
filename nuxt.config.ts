1b93e3d702cf9e0dd73d72a9f1c309c8bed66549
Vadim <spicyn.v2001@gmail.com>
2023-06-18 16:52:17 +0700

    feat/config: added configs for prettier, added and injsected into bundle bootsrap, also injected global override file related to scss, added pinit
    Generally not much of usefult just bunch of needed packages, pobably will be added some more

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    // ...
+    '@pinia/nuxt',
+  ],
+  typescript: { strict: true },
+  css: ['@@/node_modules/bootstrap/dist/css/bootstrap.min.css', '@@/scss/custom.scss'],
+  plugins: [{ src: '~/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js', mode: 'client' }],
 })
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template lang>
+  <div class="container">
+    <h1>How is it Going?</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      kek: ''
+    }
+  },
+}
+</script>
